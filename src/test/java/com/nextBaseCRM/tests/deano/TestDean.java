6b5d3b670fb4444f5a9125811d8dd0e61a00996c
Dean Wynn <wynnkhalifa117@gmail.com>
2020-12-29 11:20:55 -0600

    Added a String statement

--- src/test/java/com/nextBaseCRM/tests/deano/TestDean.java
+++ src/test/java/com/nextBaseCRM/tests/deano/TestDean.java
@@ -5,6 +5,7 @@ public class TestDean {
     public static void main(String[] args) {
 
         System.out.println("Did this work?");
+        System.out.println("What kind of Pizza do y'all like?");
 
     }
 
