456f9bc1dd1c12c5b82eb68f7c2c6f8315aca336
Mayasa79 <mayasa.sabie@gmail.com>
2020-12-29 11:24:37 -0600

    Hello Adnan

--- src/test/java/com/nextBaseCRM/tests/Mayasa/TestMayasa.java
+++ src/test/java/com/nextBaseCRM/tests/Mayasa/TestMayasa.java
@@ -4,5 +4,6 @@ public class TestMayasa {
     public static void main(String[] args) {
         System.out.println("Hello guys");
         System.out.println("HI");
+        System.out.println("Adnan");
     }
 }
